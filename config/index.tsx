c23e20b5d552c5f9141fcfc678f11629a36ab22d
jeremiah-eth <ihejierechibueze@gmail.com>
2025-11-29 15:22:23 -0500

    Create Reown Config

---
+++ config/index.tsx
@@ -0,0 +1,25 @@
+import { cookieStorage, createStorage, http } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
+import { Chain, getDefaultConfig } from '@reown/appkit/networks'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+
+// Get projectId from https://cloud.reown.com
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+if (!projectId) {
+    throw new Error('Project ID is not defined')
+}
+
+export const networks = [mainnet, sepolia] as [Chain, ...Chain[]]
+
+// Set up Wagmi Adapter
+export const wagmiAdapter = new WagmiAdapter({
+    storage: createStorage({
+        storage: cookieStorage
+    }),
+    ssr: true,
+    projectId,
+    networks
+})
+
+export const config = wagmiAdapter.wagmiConfig
