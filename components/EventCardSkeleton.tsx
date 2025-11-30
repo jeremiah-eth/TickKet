b8f44f86248b54ee5ed11dfd585c5d51c31450d1
jeremiah-eth <ihejierechibueze@gmail.com>
2025-11-30 01:30:17 -0500

    Create EventCardSkeleton component

--- components/EventCardSkeleton.tsx
+++ components/EventCardSkeleton.tsx
@@ -1,16 +1,41 @@
-import { Card } from './ui/Card'
-import { Skeleton } from './ui/Skeleton'
+'use client'
+
+import { Skeleton } from '@/components/ui/Skeleton'
+import { Card } from '@/components/ui/Card'
 
 export function EventCardSkeleton() {
     return (
-        <Card>
-            <Skeleton className="aspect-video w-full mb-4" />
-            <Skeleton className="h-6 w-3/4 mb-2" />
-            <Skeleton className="h-4 w-full mb-4" />
-            <div className="flex items-center justify-between">
-                <Skeleton className="h-5 w-20" />
-                <Skeleton className="h-4 w-24" />
+        <Card className="overflow-hidden h-full flex flex-col">
+            <div className="relative aspect-video">
+                <Skeleton className="h-full w-full" />
+            </div>
+            <div className="p-5 flex flex-col flex-grow space-y-3">
+                <div className="flex justify-between items-start">
+                    <div className="space-y-2 flex-1">
+                        <Skeleton className="h-6 w-3/4" />
+                        <Skeleton className="h-4 w-1/2" />
+                    </div>
+                    <Skeleton className="h-8 w-16 rounded-full" />
+                </div>
+                <div className="space-y-2 pt-2">
+                    <Skeleton className="h-4 w-full" />
+                    <Skeleton className="h-4 w-2/3" />
+                </div>
+                <div className="pt-4 mt-auto flex justify-between items-center">
+                    <Skeleton className="h-4 w-24" />
+                    <Skeleton className="h-9 w-24 rounded-lg" />
+                </div>
             </div>
         </Card>
+    )
+}
+
+export function EventListSkeleton() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {Array.from({ length: 6 }).map((_, i) => (
+                <EventCardSkeleton key={i} />
+            ))}
+        </div>
     )
 }
